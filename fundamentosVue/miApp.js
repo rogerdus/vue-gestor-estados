e5b023cc34b325a6eb747441d5bc7d247c6e1c9f
rogerdus4 <rogerdus@gmail.com>
2024-03-27 17:59:49 -0600

    v-for

--- fundamentosVue/miApp.js
+++ fundamentosVue/miApp.js
@@ -5,6 +5,13 @@ createApp({
     return {
         titulo: 'Nueva Instancia: Mi app - Directivas',
         aparece: false,
+        frutas: ['Manzana', 'Pera', 'Banana','Uva'],
+        ingredientes: [
+          {'id': '001', 'name': 'Pan'},
+          {'id': '002', 'name': 'Arroz'},
+          {'id': '003', 'name': 'Aceite'},
+          {'id': '004', 'name': 'Donas'},
+        ]
     };
   },
 }).mount("#miApp");
