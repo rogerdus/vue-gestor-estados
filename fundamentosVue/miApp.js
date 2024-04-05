03293bef4fbdd759c409d5f3a2aebccd38bc5a55
rogerdus4 <rogerdus@gmail.com>
2024-04-05 17:49:29 -0600

    propiedades de Vue

--- fundamentosVue/miApp.js
+++ fundamentosVue/miApp.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-        titulo: 'Nueva Instancia: Mi app - Directivas',
+        titulo: 'Nueva Instancia: Mi app - Directivas ',
         aparece: false,
         frutas: ['Manzana', 'Pera', 'Banana','Uva'],
         ingredientes: [
@@ -11,7 +11,26 @@ createApp({
           {'id': '002', 'name': 'Arroz'},
           {'id': '003', 'name': 'Aceite'},
           {'id': '004', 'name': 'Donas'},
-        ]
+        ],
+        contador: 10,
+        numero1: 0,
+        numero2: 0,
+        suma: 0,
     };
+  },
+  methods: {
+    saludar: function () {
+      console.log("saludar desde el metodo");
+    },
+    probandoParametros: function (a, b) {
+      console.log("a: " + a + " b: " + b + " = "+ a + b);
+    },
+    incrementarContador: function () {
+      this.contador = this.contador + 10;
+    },
+    sumarNumeros: function () {
+      this.suma = parseInt(this.numero1) + parseInt(this.numero2);
+    }
+
   },
 }).mount("#miApp");
---
+++ fundamentosVue/propiedades.js
@@ -0,0 +1,66 @@
+const { createApp } = Vue;
+
+createApp({
+    computed: {
+        montoPagar: function () {
+            var total = 0;
+            for (let i = 0; i < this.precios.length; i++) {
+                const monto = this.precios[i] * this.cantidades[i];
+                total += monto;
+            }
+            return total * parseFloat(this.precioDollar);
+        }
+    },
+    data() {
+        return {
+            precios: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 150],
+            cantidades: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 175],
+            productos: [
+                'zapatos',
+                'camisetas',
+                'remeras',
+                'pantalones',
+                'gorras',
+                'zapatillas',
+                'camisas',
+                'pantalones',
+                'lo que sea',
+                'zapatos',
+            ],
+            precioDollar: 18,
+            kwarg: '',
+            full_name: '',
+            direccion: '',
+            comprobante: '',
+
+        }
+    },
+    watch: {
+        kwarg: function(antiguo,nuevo){
+            console.log(val);
+
+        }
+    },
+    methods: {
+        SumarVentas() {
+            var total = 0;
+            for (let i = 0; i < this.precios.length; i++) {
+                const element = this.precios[i];
+                const monto = this.precios[i] * this.cantidades[i];
+                total += monto;
+            }
+            return total;
+        },
+        buscarProducto: function(){
+            return lista_filtrada = this.productos.filter(
+                function(valor){
+                    return valor.includes(this.kwarg);
+                }
+            );
+        }
+    },
+    mounted () {
+        console.log("carga el vue ");
+    },
+
+}).mount('#propiedadesApp');
